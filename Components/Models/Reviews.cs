cbbb231144e4e5454115b14618efa62b86441c13
sheng.huang <sheng.huang@ndsu.edu>
2025-12-16 18:36:24 -0600

    Reviews

---
+++ Components/Models/Reviews.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaFinalProject.Components.Models
+{
+    public class Reviews
+    {
+        public int Id { get; set; }
+        public int Customer_Id { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime DatePosted { get; set; }
+
+        public User User { get; set; }
+    }
+}
