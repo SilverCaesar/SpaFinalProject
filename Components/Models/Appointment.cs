7c6ce2ee7106f548dfa4aedfdfc8c6615a032115
SilverCaesar <juliusrosalin@gmail.com>
2025-12-16 21:12:51 -0600

    end 12/16

--- Components/Models/Appointment.cs
+++ Components/Models/Appointment.cs
@@ -6,58 +6,20 @@ namespace SpaFinalProject.Components.Models
 {
     public class Appointment
     {
-        [Key]
-        public int Appointment_Id { get; set; }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        // We should add status functionality in later if we have time, but for now it is never used
+        public string Status { get; set; }
 
-        [Required]
-        public string Customer_Id { get; set; } = string.Empty;
-
-        [Required]
-        public int Employee_Id { get; set; }
-
-        [Required]
-        public int Service_Id { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime Date { get; set; }
-
-        [DataType(DataType.Time)]
-        public DateTime Start { get; set; }
-
-        public string? Status { get; set; }
-
-        public string? Notes { get; set; }
-
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
-
-        public int Duration { get; set; }
-
-        public Appointment()
+        public string Notes
         {
-        }
+            get; set;
 
-        public Appointment(
-            string customerId,
-            int employeeId,
-            int serviceId,
-            DateTime date,
-            DateTime start,
-            decimal price = 0m,
-            int duration = 0,
-            string? status = null,
-            string? notes = null)
-        {
-            Customer_Id = customerId ?? string.Empty;
-            Employee_Id = employeeId;
-            Service_Id = serviceId;
-            Date = date;
-            Start = start;
-            Price = price;
-            Duration = duration;
-            Status = status;
-            Notes = notes;
         }
     }
 }
--- Components/Models/User.cs
+++ Components/Models/User.cs
@@ -5,11 +5,11 @@ namespace SpaFinalProject.Components.Models
 {
     public abstract class User
     {
-        public int User_Id { get; set; }
-
+        public int Id { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
 
-        public string? Phone { get; set; }
     }
 }
